f5e02148a8af705818674453f1b890904a92953e
Bharat Gupta <bindassbharat311@gmail.com>
2023-06-23 18:01:03 +0530

    Add JS libs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import $ from "jquery";
+import Rails from '@rails/ujs';
+
+import "merge";
+
+Rails.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.5/lib/assets/compiled/rails-ujs.js"
+
+pin "merge"
